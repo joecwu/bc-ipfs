92bb55c43090a10e45455c0b75f344247dbdf2a1
Joe Wu <joe@joecwu.com>
2018-11-30 16:16:01 +0800

    bug fix, window open no target error

--- bc-ipfs/src/FileListItem.js
+++ bc-ipfs/src/FileListItem.js
@@ -21,7 +21,7 @@ class FileListItem extends Component {
   handleAccessFile(event) {
     if(this.state.btn_access_state == 'accessed') {
       // open file directly
-      window.open('http://ipfs.io/ipfs/' + this.state.bc_resp_hash, target="_blank");
+      window.open('http://ipfs.io/ipfs/' + this.state.bc_resp_hash, "_blank");
     } else {
       this.bcAccessFile(event);
     }
