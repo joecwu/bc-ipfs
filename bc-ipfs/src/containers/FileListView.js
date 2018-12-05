23db11d7fec4021f6401e26052043444b5c7fb4c
Joe Wu <joe@joecwu.com>
2018-12-05 13:25:28 +0800

    apply Panel layout

--- bc-ipfs/src/containers/FileListView.js
+++ bc-ipfs/src/containers/FileListView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
+import { Tabs, Tab, Panel } from 'react-bootstrap';
 import FileList from '../components/FileList';
 import './App.css'; //TODO: defien FileListView own CSS
 
@@ -10,14 +10,33 @@ class FileListView extends Component {
   render() {
     /*jshint ignore:start*/
     return (
-        <Tabs defaultActiveKey="data" bsStyle="tabs" animation={false} id="filelist-tab" mountOnEnter={true} unmountOnExit={true}>
-            <Tab eventKey="data" title="Data">
+      <div className="App">
+        <header className="App-header">
+            <h1>IPFS File List</h1>
+        </header>
+        <Panel bsStyle="success">
+          <Panel.Heading>
+            <Panel.Title componentClass="h3">File Access by Search</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body>
+            <Tabs
+              defaultActiveKey="data"
+              bsStyle="tabs"
+              animation={true}
+              id="filelist-tab"
+              mountOnEnter={true}
+              unmountOnExit={true}
+            >
+              <Tab eventKey="data" title="Data">
                 <FileList pageSize={5} hideFields={['category']} category="data" />
-            </Tab>
-            <Tab eventKey="code" title="Code">
+              </Tab>
+              <Tab eventKey="code" title="Code">
                 <FileList pageSize={5} hideFields={['category']} category="code" />
-            </Tab>
-        </Tabs>
+              </Tab>
+            </Tabs>
+          </Panel.Body>
+        </Panel>
+      </div>
     );
     /*jshint ignore:end*/
   }
