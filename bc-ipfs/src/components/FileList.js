ebe2628a1024180f01b2c1b6ae00176e8b8657d8
Joe Wu <joe@joecwu.com>
2018-12-14 18:40:04 +0800

    add loading icon in file search

--- bc-ipfs/src/components/FileList.js
+++ bc-ipfs/src/components/FileList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'whatwg-fetch';
-import { Button, Form, FormGroup, ControlLabel, FormControl, Table } from 'react-bootstrap';
+import { Button, Form, FormGroup, Image, FormControl, Table } from 'react-bootstrap';
 import FileListItem from './FileListItem';
 
 var PropTypes = require('prop-types');
@@ -24,6 +24,7 @@ class FileList extends Component {
       pageIndex: props.pageIndex,
       pageSize: props.pageSize,
       sortedBy: props.sortedBy,
+      is_loading: false,
     };
     this.fetchItemsFromServer = this.fetchItemsFromServer.bind(this);
     this.search = this.search.bind(this);
@@ -32,6 +33,7 @@ class FileList extends Component {
 
   fetchItemsFromServer() {
     console.log('fetchItemsFromServer');
+    this.setState({ ['is_loading']: true });
     var targetSearchTemplate = 'blockmed-ipfs';
     window
       .fetch('https://es.blcksync.info/blockmed-ipfs/_search/template', {
@@ -46,7 +48,7 @@ class FileList extends Component {
             query_string: this.state.keyword,
             category: this.state.category,
             size: this.state.pageSize,
-            from: this.state.pageIndex * this.state.pageSize, 
+            from: this.state.pageIndex * this.state.pageSize,
             sort_by_time: this.state.sortedBy == 'time' ? 'true' : '',
             sort_by_filesize: this.state.sortedBy == 'filesize' ? 'true' : '',
             sort_by_accessed: this.state.sortedBy == 'accessed' ? 'true' : '',
@@ -54,7 +56,9 @@ class FileList extends Component {
         }),
       })
       .then(response => response.json())
-      .then(esResponse => this.convertToItems(esResponse));
+      .then(esResponse => this.convertToItems(esResponse))
+      .catch(error => console.error(error))
+      .then(() => this.setState({ ['is_loading']: false }));
   }
 
   componentDidMount() {
@@ -84,7 +88,10 @@ class FileList extends Component {
         fileSize: hitItem._source.metadata.filesize,
         tokenCost: hitItem._source.tokenCost,
         metadataCaptureTime: new Date(hitItem._source.metadataCaptureTime),
-        latestPurchaseTime: (typeof hitItem._source.latestPurchaseTime === 'undefined') ? undefined : new Date(hitItem._source.latestPurchaseTime),
+        latestPurchaseTime:
+          typeof hitItem._source.latestPurchaseTime === 'undefined'
+            ? undefined
+            : new Date(hitItem._source.latestPurchaseTime),
         noOfAccessed: hitItem._source.purchaseTxRecords.length,
       });
     });
@@ -124,6 +131,12 @@ class FileList extends Component {
             <Button type="submit" onClick={this.search}>
               Search
             </Button>
+            <Image
+              src="loading.gif"
+              height="30px"
+              width="30px"
+              style={{ display: !this.state.is_loading ? 'none' : 'inline' }}
+            />
           </FormGroup>
         </Form>
         <Table responsive striped bordered condensed hover>
@@ -132,13 +145,17 @@ class FileList extends Component {
               {this.props.hideFields.includes('accessFile') ? null : <th style={{ width: '85px' }} />}
               {this.props.hideFields.includes('description') ? null : <th>Description</th>}
               {this.props.hideFields.includes('category') ? null : <th style={{ width: '100px' }}>Category</th>}
-              {this.props.hideFields.includes('metadataCaptureTime') ? null : <th style={{ width: '150px' }}>Register Time</th>}
+              {this.props.hideFields.includes('metadataCaptureTime') ? null : (
+                <th style={{ width: '150px' }}>Register Time</th>
+              )}
               {this.props.hideFields.includes('fileSize') ? null : <th style={{ width: '150px' }}>File Size</th>}
               {this.props.hideFields.includes('tokenCost') ? null : <th style={{ width: '150px' }}>BMD Token Cost</th>}
               {this.props.hideFields.includes('noOfAccessed') ? null : (
                 <th style={{ width: '120px' }}>No. of Accessed</th>
               )}
-              {this.props.hideFields.includes('latestPurchaseTime') ? null : <th style={{ width: '150px' }}>Last Access Time</th>}
+              {this.props.hideFields.includes('latestPurchaseTime') ? null : (
+                <th style={{ width: '150px' }}>Last Access Time</th>
+              )}
             </tr>
           </thead>
           <tbody>{rows}</tbody>
