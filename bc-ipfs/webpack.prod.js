e9b123cb9afdbd6a397e05e16693ce6bfc927f53
matr1xc0in <>
2018-12-25 09:29:54 -0800

    Prepare for production distribution with nginx

--- bc-ipfs/webpack.prod.js
+++ bc-ipfs/webpack.prod.js
@@ -14,4 +14,9 @@ module.exports = merge(common, {
     historyApiFallback: true,
     inline: false,
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
 });
