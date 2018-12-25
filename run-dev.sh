e68f5d898e3b300343127d5b0176ea0ebc052e6b
matr1xc0in <>
2018-12-25 14:02:22 -0800

    Prefix docker images with blcksync namespace ID

--- run-dev.sh
+++ run-dev.sh
@@ -9,5 +9,5 @@ docker run --rm -it \
   --publish 127.0.0.1:5001:5001 \
   --publish 127.0.0.1:8888:8080 \
   --mount type=bind,source=$CURR_DIR/bc-ipfs,target=/home/ipfsuser/bc-ipfs \
-  bc-geth-ipfs-dev-${GO_VER} \
+  blcksync/bc-geth-ipfs-dev-${GO_VER} \
   bash -l
