14508f7afb9bc2c0b5bf34f87d587cdc46116764
Joe Wu <joe@joecwu.com>
2019-01-13 20:22:23 +0800

    support custom sorting in time, file size, accessed columns.

--- bc-ipfs/src/components/FileList.js
+++ bc-ipfs/src/components/FileList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'whatwg-fetch';
-import { Button, Form, FormGroup, Image, FormControl, Table } from 'react-bootstrap';
+import { Button, Form, FormGroup, Image, FormControl, Table, Pager } from 'react-bootstrap';
 import FileListItem from './FileListItem';
 
 var PropTypes = require('prop-types');
@@ -24,17 +24,62 @@ class FileList extends Component {
       pageIndex: props.pageIndex,
       pageSize: props.pageSize,
       sortedBy: props.sortedBy,
+      sortedByAssending: false,
       is_loading: false,
     };
     this.fetchItemsFromServer = this.fetchItemsFromServer.bind(this);
     this.search = this.search.bind(this);
     this.keywordChange = this.keywordChange.bind(this);
+    this.sortByColumn = this.sortByColumn.bind(this);
+    this.getSortedColumnCss = this.getSortedColumnCss.bind(this);
+  }
+
+  sortByColumn(e) {
+    console.debug('sort by column', e.target);
+    var sortedByAssending = this.state.sortedByAssending;
+    if(e.target.id == this.state.sortedBy) {
+      sortedByAssending = !sortedByAssending;
+    } else {
+      // default is desc
+      sortedByAssending = false;
+    }
+    this.setState({ ['sortedBy']: e.target.id, ['sortedByAssending']: sortedByAssending }, () => {
+      this.fetchItemsFromServer();
+    });
+  }
+
+  getSortedColumnCss(target) {
+    var css = 'sortable';
+    if(this.state.sortedBy != target) {
+      css = 'sortable';
+    } else {
+      if(this.state.sortedByAssending) {
+        css = 'sortable asc';
+      } else {
+        css = 'sortable desc';
+      }
+    }
+    return css;
   }
 
   fetchItemsFromServer() {
     console.log('fetchItemsFromServer');
     this.setState({ ['is_loading']: true });
     var targetSearchTemplate = 'blockmed-ipfs';
+    var search_req_body = JSON.stringify({
+      id: targetSearchTemplate,
+      params: {
+        query_string: this.state.keyword,
+        category: this.state.category,
+        size: this.state.pageSize,
+        from: this.state.pageIndex * this.state.pageSize,
+        sort_by_time: this.state.sortedBy == 'time' ? 'true' : '',
+        sort_by_filesize: this.state.sortedBy == 'filesize' ? 'true' : '',
+        sort_by_accessed: this.state.sortedBy == 'accessed' ? 'true' : '',
+        sort_order: this.state.sortedByAssending ? 'asc' : 'desc',
+      },
+    });
+    console.log('fetch es', search_req_body);
     window
       .fetch(CONFIG.es.search_url.blockmed_ipfs, {
         method: 'POST',
@@ -42,18 +87,7 @@ class FileList extends Component {
           Authorization: CONFIG.es.authorization,
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          id: targetSearchTemplate,
-          params: {
-            query_string: this.state.keyword,
-            category: this.state.category,
-            size: this.state.pageSize,
-            from: this.state.pageIndex * this.state.pageSize,
-            sort_by_time: this.state.sortedBy == 'time' ? 'true' : '',
-            sort_by_filesize: this.state.sortedBy == 'filesize' ? 'true' : '',
-            sort_by_accessed: this.state.sortedBy == 'accessed' ? 'true' : '',
-          },
-        }),
+        body: search_req_body,
       })
       .then(response => response.json())
       .then(esResponse => this.convertToItems(esResponse))
@@ -144,19 +178,19 @@ class FileList extends Component {
             />
           </FormGroup>
         </Form>
-        <Table responsive striped bordered condensed hover>
+        <Table responsive striped bordered condensed hover className={'table-sortable'}>
           <thead>
             <tr>
               {this.props.hideFields.includes('accessFile') ? null : <th style={{ width: '85px' }} />}
               {this.props.hideFields.includes('description') ? null : <th>Description</th>}
               {this.props.hideFields.includes('category') ? null : <th style={{ width: '100px' }}>Category</th>}
               {this.props.hideFields.includes('metadataCaptureTime') ? null : (
-                <th style={{ width: '150px' }}>Register Time</th>
+                <th id='time' className={this.getSortedColumnCss('time')} style={{ width: '150px' }} onClick={this.sortByColumn}>Register Time</th>
               )}
-              {this.props.hideFields.includes('fileSize') ? null : <th style={{ width: '150px' }}>File Size</th>}
+              {this.props.hideFields.includes('fileSize') ? null : <th id='filesize' className={this.getSortedColumnCss('filesize')} style={{ width: '150px' }} onClick={this.sortByColumn}>File Size</th>}
               {this.props.hideFields.includes('tokenCost') ? null : <th style={{ width: '150px' }}>BMD Token Cost</th>}
               {this.props.hideFields.includes('noOfAccessed') ? null : (
-                <th style={{ width: '120px' }}>No. of Accessed</th>
+                <th id='accessed' className={this.getSortedColumnCss('accessed')} style={{ width: '150px' }} onClick={this.sortByColumn}>No. of Accessed</th>
               )}
               {this.props.hideFields.includes('latestPurchaseTime') ? null : (
                 <th style={{ width: '150px' }}>Last Access Time</th>
